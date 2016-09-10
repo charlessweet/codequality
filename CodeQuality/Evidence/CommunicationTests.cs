9ec66a1c3a6336434cf30325477eb3521a3d6d53
Chuck Sweet <chuck.sweet@gmail.com>
2016-09-10 10:05:39 -0700

    Added the remaining examples to github.

--- CodeQuality/Evidence/CommunicationTests.cs
+++ CodeQuality/Evidence/CommunicationTests.cs
@@ -1,24 +1,25 @@
 ﻿
 using LaughingMonkey.CodeQuality.Communication;
+using LaughingMonkey.CodeQuality.Design;
 using NUnit.Framework;
 
 namespace Evidence
 {
     [TestFixture]
-    public class CommunicationTests
+    public class EvidenceTests
     {
         [Test]
-        public void Example1()
+        public void Example1_HumanReadable()
         {
-            ExampleOne t = new ExampleOne();
+            Example1_HumanReadable t = new Example1_HumanReadable();
             Assert.AreEqual(t.MachineOnly(), t.HumanReadable());
         }
 
         [Test]
-        public void Example2()
+        public void Example2_DeveloperDialect()
         {
-            ExampleTwo.FirstDialect_SensorXmlPacket t = new ExampleTwo.FirstDialect_SensorXmlPacket();
-            ExampleTwo.SecondDialect_SensorXmlPacket u = new ExampleTwo.SecondDialect_SensorXmlPacket();
+            Example2_DeveloperDialect.FirstDialect_SensorXmlPacket t = new Example2_DeveloperDialect.FirstDialect_SensorXmlPacket();
+            Example2_DeveloperDialect.SecondDialect_SensorXmlPacket u = new Example2_DeveloperDialect.SecondDialect_SensorXmlPacket();
             for (int i = 0; i < 10; i++)
             {
                 string nodeId = "Node" + i;
@@ -37,6 +38,36 @@ namespace Evidence
             Assert.IsFalse(u.addPayload("Fail to add this too"));
 
             Assert.AreEqual(u.render(), t.RenderPacket());
+        }
+
+        [Test]
+        public void Example3_DevelopersSometimesMumble()
+        {
+            Example3_DevelopersSometimesMumble t = new LaughingMonkey.CodeQuality.Communication.Example3_DevelopersSometimesMumble();
+            string s = t.TrivialMumblingExample();
+            Assert.AreEqual("World", s);
+        }
+
+        [Test]
+        public void Example4_DevelopersCanBeVague()
+        {
+            Example4_DevelopersCanBeVague t = new LaughingMonkey.CodeQuality.Communication.Example4_DevelopersCanBeVague();
+            Example4_DevelopersCanBeVague.Envelope testEnvelope = new LaughingMonkey.CodeQuality.Communication.Example4_DevelopersCanBeVague.Envelope();
+            testEnvelope.EnvelopeId = 1;
+            testEnvelope.Content = "This is the envelope from the database.";
+            Assert.IsTrue(t.Validate(testEnvelope));
+        }
+
+        [Test]
+        public void Example5_DevelopersTalkALot()
+        {
+            Example5_DevelopersTalkALot.UnfortunatelyLongClass t = new LaughingMonkey.CodeQuality.Design.Example5_DevelopersTalkALot.UnfortunatelyLongClass();
+            string actual = t.AddTwoNumbersRepresentedAsStringsAndConvertBases("1000", "2020", "10", "10");
+            Assert.AreEqual("3020", actual);
+
+            Example5_DevelopersTalkALot.RefactoredToShorterClass t2 = new LaughingMonkey.CodeQuality.Design.Example5_DevelopersTalkALot.RefactoredToShorterClass();
+            actual = t2.AddTwoNumbersRepresentedAsStringsAndConvertBases("1000", "2020", "10", "10");
+            Assert.AreEqual("3020", actual);
         }
     }
 }
